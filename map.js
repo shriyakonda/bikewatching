50b817e681a5a003aac14b4b71d219229819c17f
shriyakonda <166396390+shriyakonda@users.noreply.github.com>
2026-05-15 22:43:55 -0700

    h

--- map.js
+++ map.js
@@ -12,3 +12,38 @@ const map = new mapboxgl.Map({
   minZoom: 5,
   maxZoom: 18,
 });
+
+// shared style so I don't have to repeat it for both cities
+const bikeLaneStyle = {
+  'line-color': '#32D400',
+  'line-width': 3,
+  'line-opacity': 0.5,
+};
+
+map.on('load', async () => {
+  // boston bike lanes
+  map.addSource('boston_route', {
+    type: 'geojson',
+    data: 'https://bostonopendata-boston.opendata.arcgis.com/datasets/boston::existing-bike-network-2022.geojson',
+  });
+
+  map.addLayer({
+    id: 'bike-lanes',
+    type: 'line',
+    source: 'boston_route',
+    paint: bikeLaneStyle,
+  });
+
+  // cambridge bike lanes
+  map.addSource('cambridge_route', {
+    type: 'geojson',
+    data: 'https://raw.githubusercontent.com/cambridgegis/cambridgegis_data/main/Recreation/Bike_Facilities/RECREATION_BikeFacilities.geojson',
+  });
+
+  map.addLayer({
+    id: 'bike-lanes-cambridge',
+    type: 'line',
+    source: 'cambridge_route',
+    paint: bikeLaneStyle,
+  });
+});
