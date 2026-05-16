837f090fd9a74278623a0f80f7cc9489d8ae2078
shriyakonda <166396390+shriyakonda@users.noreply.github.com>
2026-05-15 22:41:45 -0700

    g
    u

---
+++ map.js
@@ -0,0 +1,14 @@
+import mapboxgl from 'https://cdn.jsdelivr.net/npm/mapbox-gl@2.15.0/+esm';
+import * as d3 from 'https://cdn.jsdelivr.net/npm/d3@7.9.0/+esm';
+
+console.log('Mapbox GL JS Loaded:', mapboxgl);
+
+mapboxgl.accessToken = 'pk.eyJ1Ijoic2hyaXlha29uZGEiLCJhIjoiY21wN3gyNnY1MDZ0eDMyb2ptNDVuMG1ndCJ9.PJIgMQvNXexH5kglQYmQTQ';
+const map = new mapboxgl.Map({
+  container: 'map',
+  style: 'mapbox://styles/mapbox/streets-v12',
+  center: [-71.09415, 42.36027],
+  zoom: 12,
+  minZoom: 5,
+  maxZoom: 18,
+});
